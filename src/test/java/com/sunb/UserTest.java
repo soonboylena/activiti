863b76e9b68cb570d9f43bcea6c35de624a06976
soonboylena <17131007@qq.com>
2017-04-21 18:23:13 +0800

    test

--- src/test/java/com/sunb/UserTest.java
+++ src/test/java/com/sunb/UserTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.Assert;
 
+import java.util.List;
+
 /**
  * Created by sunb on 17-4-21.
  */
@@ -30,6 +32,22 @@ public class UserTest {
         admin.setPassword("sunbo");
         identityService.saveUser(admin);
 
-        Assert.notNull(identityService.getUserInfoKeys("sunbo"),"没找到用户sunbo");
+        Assert.notNull(identityService.getUserInfoKeys("sunbo"), "没找到用户sunbo");
     }
+
+    @Test
+    public void userKeyTest() {
+
+        User user = identityService.createUserQuery().userId("sunbo").singleResult();
+        user.setLastName("xxxxx");
+        identityService.saveUser(user);
+
+        List<String> keys = identityService.getUserInfoKeys("sunbo");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        for (String key : keys) {
+            String info = identityService.getUserInfo("sunbo", key);
+            System.out.println(key + ":" + info);
+        }
+    }
+
 }
